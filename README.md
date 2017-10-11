a08dc9751998767680e41371f565db2d6822729a
Santiago <santiago.correa.dev@gmail.com>
2017-10-11 17:12:53 -0500

    Bug Fixes : Use of dt

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # How To Play
-* Just open up index.html
-* You can also open up https://santiago-correa-dev.github.io/frogger/ in your browser and start playing.
+* You can download the game with the following link : [Frogger](https://github.com/Santiago-Correa-Dev/frogger.git).
+* Once downloaded just open up the index.html file and play on your browser.
+* Or you can also open up the game  by clicking [here](https://santiago-correa-dev.github.io/frogger/) in your browser and start playing.
+* All you need to to do to play is have a computer, a browser, a working keyboard and internet. Have FUN!
 
 ## Game Instructions
 * To start playing use the up, down , left and right keys to move the character.
 * The game will start automatically waiting on you to play.
-* If you win you get an alert saying - You win!
-* If you lose you get an alert saying - You lose!
+* If you win you get a point.
+* If you lose you lose a point.
 
 ### Game Details
 In this game you have a Player and Enemies (Bugs). 
--- js/app.js
+++ js/app.js
@@ -1,64 +1,64 @@
 // Enemies our player must avoid
 class Enemy {
 
-    constructor(x, y, speed, points) {
+    constructor(x, y, points, speed) {
         this.x = x;
         this.y = y;
-        this.speed = Math.random() * 3 + 1;
         this.sprite = 'images/enemy-bug.png';
         this.points = 0;
-    };
+        this.speed = Math.random() + 2 * 1;
+    }
     // Parameter: dt, a time delta between ticks
     update(dt) {
-        this.speed * dt;
-        this.x += this.speed;
+
+         this.x += dt * Math.random() + 2  * 1 + this.speed;
         if (this.x >= 505) {
             this.x = 0;
-        };
+        }
 
-        if (this.x >= player.x - 20 && this.x <= player.x + 20 && this.y >= player.y - 10 && this.y <= player.y + 10) {
+        if (this.x >= player.x - 50 && this.x <= player.x + 50 && this.y >= player.y - 10 && this.y <= player.y + 10) {
             player.reset();
             this.x = 0;
             player.score--;
-        };
-    };
+        }
+    }
     // Draw the enemy on the screen, required method for game
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    };
+    }
 
-};
+}
 
 
 // Draws Player on the screen, required method for game
 class Player extends Enemy {
     constructor() {
-        super()
+        super();
         this.x = 200;
         this.y = 300;
         this.sprite = 'images/char-boy.png';
         this.score = 0;
-    };
+    }
 
     // Update the player's position, required method for game
     update(dt) {
-        this.speed * dt;
+        this.speed += dt;
         if (this.y < 0) {
             this.reset();
             this.score++;
-        };
+        }
 
         document.getElementById('score').innerHTML = this.score;
-    };
+    }
 
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    };
+    }
 
     reset() {
         this.x = 200;
         this.y = 300;
-    };
+    }
 
     //Handles key events and moves Player accordingly.
     handleInput(value) {
@@ -70,9 +70,9 @@ class Player extends Enemy {
             this.x -= 100;
         } else if (value === 'right' && this.x < 400) {
             this.x += 100;
-        };
+        }
     }
-};
+}
 
 
 
